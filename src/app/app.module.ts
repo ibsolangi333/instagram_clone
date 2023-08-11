385c494a949b939ebec75898cdbb550375038dff
m-ibrahim99 <ibsolangi444@gmail.com>
2023-08-12 02:56:50 +0500

    login-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login_signUp/login/login.component';
+import { SignUpComponent } from './login_signUp/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
