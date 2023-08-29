2047f7a7f2b779564f4b497bf887131bfe17d49d
m-ibrahim99 <ibsolangi444@gmail.com>
2023-08-29 17:21:30 +0500

    mobile header

--- src/app/main_components/main/home/mobile-header/mobile-header.component.ts
+++ src/app/main_components/main/home/mobile-header/mobile-header.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./mobile-header.component.css']
 })
 export class MobileHeaderComponent {
+  searcIcon:boolean = true;
+  closeIcon:boolean = false;
+  hideIcons(){
+    this.searcIcon=false;
+    this.closeIcon=true;
+  }
+  removeSearchContent(){
+    this.searcIcon=true;
+    this.closeIcon=false;
+  }
 
 }
